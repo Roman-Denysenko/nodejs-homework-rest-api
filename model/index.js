0b26fe5e1afb07c2b4f3281e28bbe328b32d4a24
Roman-Denysenko <roman.denisenko83@gmail.com>
2021-04-15 13:28:39 +0300

    Fork repo, add listContacts, getContactById, addContact function

--- model/index.js
+++ model/index.js
@@ -1,15 +1,62 @@
-// const fs = require('fs/promises')
-// const contacts = require('./contacts.json')
+const fs = require("fs/promises");
+const contacts = require("./contacts.json");
+const path = require("path");
 
-const listContacts = async () => {}
+const contactsPath = path.join(__dirname, "contacts.json");
 
-const getContactById = async (contactId) => {}
+const listContacts = async () => {
+  try {
+    const data = await fs.readFile(contactsPath);
+    const result = JSON.parse(data);
+    return result;
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+};
 
-const removeContact = async (contactId) => {}
+const getContactById = async (contactId) => {
+  try {
+    const data = await fs.readFile(contactsPath);
+    const result = JSON.parse(data).find((el) => el.id === contactId);
+    if (!result) {
+      throw err;
+    }
+    return result;
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+};
 
-const addContact = async (body) => {}
+const removeContact = async (contactId) => {};
 
-const updateContact = async (contactId, body) => {}
+const addContact = async (body) => {
+  try {
+    const { name, email, phone } = body;
+    const data = await fs.readFile(contactsPath);
+    const dataArr = JSON.parse(data);
+    const id = dataArr.length + 1;
+    const item = {
+      id: id,
+      name: `${name}`,
+      email: `${email}`,
+      phone: `${phone}`,
+    };
+
+    dataArr.push(item);
+    fs.writeFile(contactsPath, JSON.stringify(dataArr), (error) => {
+      if (err) throw err;
+    });
+    console.log("Contact add success");
+    return dataArr;
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+};
+
+const updateContact = async (contactId, body) => {};
 
 module.exports = {
   listContacts,
@@ -17,4 +64,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,24 +1,42 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
+const contacts = require("../../model/index.js");
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/", async (req, res, next) => {
+  try {
+    const result = await contacts.listContacts();
+    res.json(result);
+  } catch (err) {
+    next(err);
+  }
+});
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:contactId", async (req, res, next) => {
+  try {
+    const id = Number(req.params.contactId);
+    const result = await contacts.getContactById(id);
+    res.json(result);
+  } catch (err) {
+    res.status(404).json({ message: "Not found" });
+  }
+});
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/", async (req, res, next) => {
+  try {
+    const body = req.body;
+    const result = await contacts.addContact(body);
+    res.status(201).json(result);
+  } catch (err) {
+    next(err);
+  }
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.delete("/:contactId", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
 
-router.patch('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.patch("/:contactId", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
 
-module.exports = router
+module.exports = router;
